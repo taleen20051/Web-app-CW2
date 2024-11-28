34dfe805a88c7c1e2d2d1ab4ca7622bf7ab833b9
taleen20051 <sc23taaa@leeds.ac.uk>
2024-11-28 22:39:57 +0000

    done

--- Section_2/config.py
+++ Section_2/config.py
@@ -1,10 +1,8 @@
 import os
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'a-very-secret-secret'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'a-very-secret-secret')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = (
-    'sqlite:///' + os.path.join(basedir, 'app.db')
-)
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app.db")}')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
